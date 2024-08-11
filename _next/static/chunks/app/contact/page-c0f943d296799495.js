e4ce8720f07512b3ec0546adf9e24d1bb03dcdc3
js-tech1 <59276982+js-tech1@users.noreply.github.com>
2024-08-11 22:06:45 +0530

    Updates

---
+++ _next/static/chunks/app/contact/page-c0f943d296799495.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[327],{1778:function(e,n,s){Promise.resolve().then(s.t.bind(s,231,23)),Promise.resolve().then(s.bind(s,29))}},function(e){e.O(0,[231,568,29,971,23,744],function(){return e(e.s=1778)}),_N_E=e.O()}]);
